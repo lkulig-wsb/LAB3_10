98928c745950842076708c82bc9cbbecd58fe7aa
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-11-23 15:50:05 +0100

    Third Commit.

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,12 @@ public class Main {
         //Testowanie dodawania liczb z parametrów
         //System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
 
-
-
+        //Sprawdzenie czy podano conajmniej dwa parametry
+        if(args.length<3)
+        {
+            System.out.println("Podano za mało parametrów. Minimalna ilość to 3!");
+            return;
+        }
 
         //Pobranie liczb z argumentów
         int zmiennaInt1 = Integer.parseInt(args[0]);
@@ -20,11 +24,16 @@ public class Main {
         int wynikDodawania1 = zmiennaInt1 + zmiennaInt2 + zmiennaInt3;
         int wynikMnozenia1 = zmiennaInt2 * zmiennaInt3;
         int dzilanieNawiasy = (zmiennaInt1 + zmiennaInt3) * zmiennaInt2;
+        int wynikRoznica = zmiennaInt3 - zmiennaInt2;
+        double wynikDzielenie = (double)zmiennaInt2 / (double)zmiennaInt3;
 
         //Wyświetlenie wyniku operacji arymetycznych
         System.out.print(zmiennaInt1 + " + " + zmiennaInt2 + " + " + zmiennaInt3 + " = " + wynikDodawania1);
         System.out.print("\n"+zmiennaInt2 + " * " + zmiennaInt3 + " = "  + wynikMnozenia1);
         System.out.print("\n("+zmiennaInt1 + " + " + zmiennaInt3 + ") "  + " * " + zmiennaInt2 + " = " + dzilanieNawiasy);
-
+        System.out.print("\n"+zmiennaInt3 + " - " + zmiennaInt2 + " = "  + wynikRoznica);
+        if(zmiennaInt3 !=0){
+            System.out.print("\n"+zmiennaInt2 + " / " + zmiennaInt3 + " = "  + wynikDzielenie);
+        }else{System.out.println("\nDzielenie przez 0 jest niemożliwe!!!");}
     }
 }
